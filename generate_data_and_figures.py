f7f540f06d83b43045b7c61fc2c347d3e7ffed11
Amit Federbush <afederbush@gmail.com>
2023-07-26 12:21:16 +0300

    code for figures

--- generate_data_and_figures.py
+++ generate_data_and_figures.py
@@ -1,4 +1,4 @@
-from do_analysis import test_accuracy, test_indifference_to_initial_condition, K_most_likely_data
+from generate_data import test_accuracy, test_indifference_to_initial_condition, K_most_likely_data
 from code_for_figures import figure_1, figure_2, figure_3, figure_5, figure_6, figure_7
 
 if __name__ == '__main__':
