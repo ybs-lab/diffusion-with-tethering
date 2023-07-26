aa1225740b646692ef1db835c7e04cd8bbba59ea
Amit Federbush <afederbush@gmail.com>
2023-07-26 12:11:18 +0300

    figures_generating_code

---
+++ generate_data_and_figures.py
@@ -0,0 +1,13 @@
+from do_analysis import test_accuracy, test_indifference_to_initial_condition, K_most_likely_data
+from code_for_figures import figure_1, figure_2, figure_3, figure_5, figure_6, figure_7
+
+if __name__ == '__main__':
+    # test_indifference_to_initial_condition()
+    # test_accuracy()
+    # K_most_likely_data()
+    # figure_1()
+    # figure_2()
+    # figure_3()
+    # figure_5()
+    # figure_6()
+    figure_7()
